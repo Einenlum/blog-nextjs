797db6562d7d47116f4bdfae7ebbed2af85b5cdd
Yann Rabiller <yann.rabiller@fortrabbit.com>
2022-07-15 17:27:56 +0200

    Fix about

--- components/header.jsx
+++ components/header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
 
     return <>
         <header>
-            <h1 className="text-right md:my-12 mb-4 mt-10 text-xl md:text-2xl font-mono">Einenlum<span className="text-pink-700 font-bold">.</span></h1>
+            <h1 className="text-right md:my-12 xl:my-16 mb-4 mt-10 text-xl md:text-2xl font-mono">Einenlum<span className="text-pink-700 font-bold">.</span></h1>
             <nav className="my-6 md:my-12 md:pr-0 pr-2">
                 <ul className="flex justify-end">
                     {elements.map((element, key) => <HeaderElement key={key} elem={element} />)}
