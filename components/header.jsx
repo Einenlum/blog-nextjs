c0df411ce75b10f1d367bce02c4d2a02d2a953bb
Yann Rabiller <yann.rabiller@fortrabbit.com>
2022-07-16 13:32:38 +0200

    Remove page title

--- components/header.jsx
+++ components/header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     ]
 
     return <>
-        <header>
+        <header className="mb-14 md:mb-20">
             <h1 className="text-right md:my-12 xl:my-16 mb-4 mt-10 text-xl md:text-2xl font-mono">Einenlum<span className="text-pink-700 font-bold">.</span></h1>
             <nav className="my-6 md:my-12 md:pr-0 pr-2">
                 <ul className="flex justify-end">
