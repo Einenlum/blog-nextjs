29b7296c6e274cb36411ceff030991c8d722a8bc
Yann Rabiller <yann.rabiller@fortrabbit.com>
2022-07-18 10:52:17 +0200

    Fix title design

--- components/index/article-link.jsx
+++ components/index/article-link.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function ArticleLink({href, children}) {
     return (
-        <h2 className="text-base font-semibold md:text-lg">
+        <h2 className="text-base tracking-wide md:text-lg">
         <Link href={href}>
             <a className="dark:hover:text-pink-600 hover:text-pink-900 transition ease-in-out">{children}</a>
         </Link>
--- components/layout.jsx
+++ components/layout.jsx
@@ -3,7 +3,7 @@ import Header from './header';
 export default function Layout({ children }) {
   return (
     <>
-      <div className="sm:mx-auto mx-3 sm:max-w-xl md:max-w-2xl 2xl:max-w-3xl mb-10 md:mb-16 lg:mb-40 xl:mb-60">
+      <div className="sm:mx-auto mx-3 sm:max-w-xl md:max-w-2xl 2xl:max-w-3xl mb-20 lg:mb-40 xl:mb-60">
         <Header />
         {children}
       </div>
