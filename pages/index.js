80972262d8962a93af31aa776554544df0b033e8
Yann Rabiller <yann.rabiller@fortrabbit.com>
2022-10-12 16:25:37 +0200

    Move getArticles in a specific repository

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,9 @@
-import fs from 'fs'
 import Head from 'next/head'
 import Layout from '../components/layout'
 import PageTitle from '../components/page-title'
 import ALink from '../components/a-link'
 import ArticlesContainer from '../components/index/articles-container'
-const fsPromises = fs.promises
-
-async function getArticles() {
-    const filenames = await fsPromises.readdir(process.cwd() + '/pages/articles')
-
-    const getMeta = async filename => {
-        const {meta} = await import(`./articles/${filename}`)
-
-        return {...meta, filename}
-    }
-
-    const metas = await Promise.all(filenames.map(filename => getMeta(filename)))
-
-    // Sort by last published
-    return metas.sort((a, b) => {
-        return b.publishedAt - a.publishedAt
-    }).map(meta => {
-        return {
-            ...meta,
-            publishedAt: meta.publishedAt.toDateString(),
-            link: `articles/${meta.filename.replace('.mdx', '')}`
-        }
-    })
-}
+import { getArticles } from '../repositories/articleRepository'
 
 export async function getStaticProps() {
     return {
---
+++ repositories/articleRepository.js
@@ -0,0 +1,25 @@
+import fs from 'fs'
+const fsPromises = fs.promises
+
+
+export async function getArticles() {
+    const filenames = await fsPromises.readdir(process.cwd() + '/pages/articles')
+    const getMeta = async filename => {
+        const {meta} = await import(`../pages/articles/${filename}`)
+
+        return {...meta, filename}
+    }
+
+    const metas = await Promise.all(filenames.map(filename => getMeta(filename)))
+
+    // Sort by last published
+    return metas.sort((a, b) => {
+        return b.publishedAt - a.publishedAt
+    }).map(meta => {
+        return {
+            ...meta,
+            publishedAt: meta.publishedAt.toDateString(),
+            link: `articles/${meta.filename.replace('.mdx', '')}`
+        }
+    })
+}
