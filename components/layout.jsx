d6c13f1a5dc545ec7c171b3ce6d9590aecb8471c
Yann Rabiller <yann.rabiller@fortrabbit.com>
2022-10-13 16:54:10 +0200

    Add feeds and article description

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,12 +1,43 @@
 import Header from './header';
+import Image from 'next/image';
+import ALink from './a-link';
+import { useState, useEffect } from 'react';
 
-export default function Layout({ children }) {
+export default function Layout({ children, showFeed = true }) {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Add listener to update styles
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => setDarkMode(e.matches));
+  
+    // Setup dark/light mode for the first time
+    setDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches)
+  
+    // Remove listener
+    return () => {
+      window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', () => {});
+    }
+  }, []);
+
+  const filename = darkMode ? 'rss-white' : 'rss';
+  const image = `/images/${filename}.png`
+  
   return (
     <>
       <div className="sm:mx-auto mx-3 sm:max-w-xl md:max-w-2xl 2xl:max-w-3xl mb-20 lg:mb-40 xl:mb-60">
         <Header />
         {children}
+
+        {
+          showFeed
+          ? <div className="mt-14 sm:mt-24 text-sm text-right mr-4">
+                <ALink href="/rss"><span><Image src={image} width="10" height="10"></Image> Feeds</span></ALink>
+            </div>
+          : null
+        }
       </div>
+
+
     </>
   );
 }
---
+++ repositories/feedGenerator.js
@@ -0,0 +1,88 @@
+import fs from "fs";
+import { Feed } from "feed"
+import { getArticles } from "./articleRepository"
+
+export const generateRssFeed = async () => {
+    const articles = await getArticles();
+    const siteURL = 'https://einenlum.com';
+    const date = new Date();
+  
+    const author = {
+  
+      name: "Yann Rabiller",
+  
+      email: "yann.rabiller@gmail.com",
+  
+      link: "https://twitter.com/Einenlum",
+  
+    };
+  
+    const feed = new Feed({
+  
+      title: "Einenlum",
+  
+      description: "Personal website and blog",
+  
+      id: siteURL,
+  
+      link: siteURL,
+    
+      favicon: `${siteURL}/favicon.png`,
+  
+      copyright: `All rights reserved ${date.getFullYear()}, Yann Rabiller`,
+  
+      updated: date,
+  
+      generator: "Feed for Node.js",
+  
+      feedLinks: {
+  
+        rss2: `${siteURL}/rss/feed.xml`,
+  
+        json: `${siteURL}/rss/feed.json`,
+  
+        atom: `${siteURL}/rss/atom.xml`,
+  
+      },
+  
+      author,
+  
+    });
+  
+    articles.forEach((article) => {
+  
+      const url = `${siteURL}/${article.link}`;
+  
+      feed.addItem({
+  
+        title: article.articleTitle,
+  
+        id: url,
+  
+        link: url,
+  
+        description: article.description,
+    
+        author: [author],
+  
+        contributor: [author],
+  
+        date: new Date(article.publishedAt),
+  
+      });
+  
+    });
+
+    return feed;
+  
+  };
+
+
+export const writeFeed = (feed) => {
+    const rootDir = process.cwd();
+
+    fs.mkdirSync(`${rootDir}/public/rss`, { recursive: true });
+    fs.writeFileSync(`${rootDir}/public/rss/feed.xml`, feed.rss2());
+    fs.writeFileSync(`${rootDir}/public/rss/atom.xml`, feed.atom1());
+    fs.writeFileSync(`${rootDir}/public/rss/feed.json`, feed.json1());
+}
