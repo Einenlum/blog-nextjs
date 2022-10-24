e9f882e11639dc4b3b70bc41ac9be41e0916e6c0
Yann Rabiller <yann.rabiller@fortrabbit.com>
2022-10-24 08:24:31 +0200

    TWIL 42

--- components/with-syntax-highlighting.jsx
+++ components/with-syntax-highlighting.jsx
@@ -5,12 +5,14 @@ import gherkin from 'highlight.js/lib/languages/gherkin'
 import yaml from 'highlight.js/lib/languages/yaml'
 import python from 'highlight.js/lib/languages/python'
 import toml from 'highlight.js/lib/languages/ini'
+import rust from 'highlight.js/lib/languages/rust'
 
 
 const mapping = {
     'php': php,
     'python': python,
     'gherkin': gherkin,
+    'rust': rust,
     'yaml': yaml,
     'yml': yaml,
     'toml': toml
