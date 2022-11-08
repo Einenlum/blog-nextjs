d5a0ea6c6f9744a9c5a1aecfd559e09dd9a1df49
Yann Rabiller <yann.rabiller@fortrabbit.com>
2022-11-09 00:19:07 +0100

    Remove duplicate styling

--- components/article-header.tsx
+++ components/article-header.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function ArticleHeader({date, children}: Props) {
     return (
-        <div className="mt-10 mb-10 xl:mb-14 pl-3 py-6 pl-6 align-bottom border-l-4 border-l-pink-900">
+        <div className="mt-10 mb-10 xl:mb-14 py-6 pl-6 align-bottom border-l-4 border-l-pink-900">
             {children}
             <ArticleDate date={date} />
         </div>
